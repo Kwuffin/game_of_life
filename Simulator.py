f549be8ccd1810b5a133cdafdc12cfb6f29e6ff4
Kwuffin <bestboykevin.bbg@gmail.com>
2020-12-01 17:28:09 +0100

    GREEN push; Code passes the unittests, added docstrings and comments.

--- Simulator.py
+++ Simulator.py
@@ -1,4 +1,5 @@
 from World import *
+from copy import deepcopy
 
 
 class Simulator:
@@ -23,11 +24,45 @@ class Simulator:
         """
         Updates the state of the world to the next generation. Uses rules for evolution.
 
+        Rules:
+        1. Every living cell with less than two neighbours die (exposure).
+        2. Every living cell with more than three neighbours die (overcrowding).
+        3. Every cell with two, or three neighbours remain unchanged (survival).
+        4. Every dead cell with three neighbours, come to life (birth).
+
+        We need to make a copy and a new instance of the world with generation n - 1, so that changes in the new world
+        don't affect changes in the same generation. Since we have two for-loops, the top-left cells in the grid will
+        be updated earlier than the cells in the bottom-right.
+
+        Then we just need to set the status of every cell according to the unittest that we wrote earlier.
+
         :return: New state of the world.
         """
         self.generation += 1
 
         # TODO: Do something to evolve the generation
+        self.worldOld = deepcopy(self.world)  # Deepcopy the world with generation n - 1.
+
+        #  For every position in the grid.
+        for x in range(self.world.width):
+            for y in range(self.world.height):
+                neighborCount = np.count_nonzero(self.worldOld.get_neighbours(x, y))  # Gets the amount of neighbours.
+
+                oldStatus = self.worldOld.get(x, y)  # Gets the status of the cell in generation n - 1.
+
+                # Set cell status for generation n.
+                if 2 <= neighborCount <= 3:
+                    if neighborCount == 3:
+                        self.world.set(x, y)
+
+                    elif neighborCount == 2:
+                        if oldStatus == 1:
+                            self.world.set(x, y)
+                        elif oldStatus == 0:
+                            self.world.set(x, y, 0)
+
+                else:
+                    self.world.set(x, y, 0)
 
         return self.world
 
